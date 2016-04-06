b54389b2226c48c6cdf887f8e9a5e456e1666745
Matthew Tse <mtse9779@gmail.com>
2016-04-05 23:57:15 -0400

    gradient speed to adapt screen height

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,10 @@
+var height = $(window).height();
+console.log(height);
+
 (function () {
     $(window).scroll(function () {
         var oVal;
-        oVal = $(window).scrollTop() / 240;
+        oVal = $(window).scrollTop() / height;
         return $('.blur').css('opacity', oVal);
     });
 }.call(this));
