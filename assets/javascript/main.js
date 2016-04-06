b91174651b7e0467b2773cfd6a3e5a754d42e773
Matthew Tse <mtse9779@gmail.com>
2016-04-05 21:52:51 -0400

    added font awesome and scroll effect

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+(function () {
+    $(window).scroll(function () {
+        var oVal;
+        oVal = $(window).scrollTop() / 240;
+        return $('.blur').css('opacity', oVal);
+    });
+}.call(this));
