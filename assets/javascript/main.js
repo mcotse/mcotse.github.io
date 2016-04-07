286a5a135d290cbdd2d1c7fcd037d025b2c5d1d2
Matthew Tse <mtse9779@gmail.com>
2016-04-07 01:44:56 -0400

    sticky navbar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,20 @@
 var height = $(window).height();
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
 (function () {
     $(window).scroll(function () {
         var oVal;
-        oVal = $(window).scrollTop() / height;
+        oVal = $(window).scrollTop() / height * 2;
         return $('.blur').css('opacity', oVal);
     });
 }.call(this));
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
