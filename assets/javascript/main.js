72ae60c59e56eea73635bb5bd4c4e3a448040808
Matthew Tse <mtse9779@gmail.com>
2016-04-11 18:24:43 -0400

    disabled parallex

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -25,25 +25,25 @@ var headroom = new Headroom(Header, {
   "tolerance": 5
 });
 headroom.init();
-
-$(document).ready(function(){
-   // cache the window object
-   $window = $(window);
-
-   $('section[data-type="background"]').each(function(){
-     // declare the variable to affect the defined data-type
-     var $scroll = $(this);
-
-      $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards
-        var yPos = -($window.scrollTop() / $scroll.data('speed'));
-
-        // background position
-        var coords = '50% '+ yPos + 'px';
-
-        // move the background
-        $scroll.css({ backgroundPosition: coords });
-      }); // end window scroll
-   });  // end section function
-}); // close out script
+//
+// $(document).ready(function(){
+//    // cache the window object
+//    $window = $(window);
+//
+//    $('section[data-type="background"]').each(function(){
+//      // declare the variable to affect the defined data-type
+//      var $scroll = $(this);
+//
+//       $(window).scroll(function() {
+//         // HTML5 proves useful for helping with creating JS functions!
+//         // also, negative value because we're scrolling upwards
+//         var yPos = -($window.scrollTop() / $scroll.data('speed'));
+//
+//         // background position
+//         var coords = '50% '+ yPos + 'px';
+//
+//         // move the background
+//         $scroll.css({ backgroundPosition: coords });
+//       }); // end window scroll
+//    });  // end section function
+// }); // close out script
