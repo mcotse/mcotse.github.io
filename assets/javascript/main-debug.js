5a02854d9a5b39fee26b82c78b0e307b85ea7654
Matthew Tse <mtse9779@gmail.com>
2017-02-06 15:10:11 -0500

    Add gulp task to build for production minifies css/js

---
+++ assets/javascript/main-debug.js
@@ -0,0 +1,28 @@
+//nav-scrolling
+$(".about-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 700);
+});
+$(".work-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 700);
+});
+$(".contact-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 700);
+});
+$(".arrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 700);
+});
+
+$('[data-typer-targets]').typer()
+
+//add breakline in between name for mobile
+if ($(window).width() <= 600) {
+  $('#mobile-breakline').after('<br>')
+}
